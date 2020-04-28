af040a5fa452de5e4fa211751d0744d2ed27c299
ghostpkmn <src402@nyu.edu>
2020-04-28 13:47:19 -0400

    Finished tentative design for music spider chart and added it to the workflow.

--- music-spider-chart.js
+++ music-spider-chart.js
@@ -1,9 +1,9 @@
 const d = {
-    "Bass": 9,
-    "Guitar": 7.5,
-    "Ukulele": 6.5,
-    "Piano": 7,
-    "Drums": 3
+    "Bass": 9.5,
+    "Guitar": 8.0,
+    "Ukulele": 7.0,
+    "Piano": 7.5,
+    "Drums": 4
 };
 
 const dOrigin = {
@@ -22,6 +22,13 @@ const radialScale = d3.scaleLinear()
     .domain([0, 10])
     .range([0, 167]);
 const ticks = [2, 4, 6, 8, 10];
+const tickRepresentation = {
+    2: "Not very good",
+    4: "Alright",
+    6: "Decent",
+    8: "Pretty comfortable",
+    10: "Let's start a band"
+};
 
 function angleToCoordinate(angle, value, scale) {
     let x = Math.cos(angle) * radialScale(value);
@@ -84,11 +91,23 @@ function drawSpiderWeb(container) {
             .attr("x", () => {
                 // TODO: FIX X POSITION OF TEXT TO BE VARIABLE
 
-                return labelCoordinate.x >= spiderWidth / 2 ? labelCoordinate.x : labelCoordinate.x - (
-                    spiderWidth / 8.5
-                );
+                if (labelCoordinate.x > spiderWidth / 2) {
+                    return labelCoordinate.x + (spiderWidth / 100);
+                } else if (labelCoordinate.x < spiderWidth / 2) {
+                    return labelCoordinate.x - (spiderWidth / 10);
+                }
+
+                return labelCoordinate.x - 10;
             })
-            .attr("y", labelCoordinate.y)
+            .attr("y", () => {
+                if (labelCoordinate.y > spiderHeight / 2) {
+                    return labelCoordinate.y + (spiderHeight / 50);
+                }
+
+                return labelCoordinate.y - (spiderHeight / 60);
+            })
+            .style('fill', '#010a43')
+            .style('font-weight', 'bold')
             .text(featureName);
     }
 }
@@ -131,20 +150,56 @@ function animateSkillPath(path) {
         });
 }
 
-function drawSpiderChart() {
-    // Create spider chart SVG
-    var spiderSVG = d3.select("#d3-spider")
-        .attr("width", spiderWidth)
-        .attr("height", spiderHeight);
-
-    // Draws spider web
-    drawSpiderWeb(spiderSVG);
-
-    // Get spider skill path
-    var skillPath = getSkillPath(spiderSVG)
-
-    // Animate skill path
-    animateSkillPath(skillPath);
+function drawTickMarkLables(container) {
+    ticks.forEach(t =>
+        container.append("text")
+        .attr("x", spiderWidth / 2 + 2.5)
+        .attr("y", spiderHeight / 2 - 1 - radialScale(t))
+        .attr("font-size", "0.5rem")
+        .attr("fill", "#56476D")
+        .attr("font-weight", "bold")
+        .text(" ↙ " + tickRepresentation[t])
+    );
 }
 
-drawSpiderChart();
+function addSpiderChartHoverListener(container) {
+    $("#music").hover(() => {
+        if (pageState === PageState.CLICK) {
+            pageState = PageState.HOVER;
+        }
+
+        $(".landing-picture").first().addClass("d-none");
+        $(".education").first().addClass("d-none");
+        $(".technical-skills").first().addClass("d-none");
+        $(".japanese").first().addClass("d-none");
+        $(".map").first().addClass("d-none");
+        $(".music").first().removeClass("d-none");
+
+        var spiderSVG = d3.select("#d3-spider")
+            .attr("width", spiderWidth)
+            .attr("height", spiderHeight);
+
+        // Draw spider web
+        drawSpiderWeb(spiderSVG);
+
+        // Add tick mark labels
+        drawTickMarkLables(spiderSVG);
+
+        // Get spider skill path
+        var skillPath = getSkillPath(spiderSVG)
+
+        // Animate skill path
+        animateSkillPath(skillPath);
+    }, () => {
+        $(".landing-picture").first().removeClass("d-none");
+        $(".education").first().addClass("d-none");
+        $(".technical-skills").first().addClass("d-none");
+        $(".japanese").first().addClass("d-none");
+        $(".map").first().addClass("d-none");
+        $(".music").first().addClass("d-none");
+
+        $("#d3-spider").empty();
+    });
+}
+
+addSpiderChartHoverListener();
